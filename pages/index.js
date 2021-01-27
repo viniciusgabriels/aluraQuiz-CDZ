7e0a2654b26b2f4ce6b21c006b79c65fd33a1741
Vinícius Gabriel <viniciusgabriel.rs@hotmail.com>
2021-01-27 17:38:59 -0300

    aula 3 sem questões e sem desafios

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,9 @@ import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
@@ -43,16 +35,15 @@ export default function Home() {
               console.log("Fazendo submissão por meio do React")
             }}
             >              
-              <input 
-                onChange={function (event) {
-                  setName(event.target.value);
-                }}  
-                placeholder="Digite seu nome" 
+              <Input
+                name="nomeDoUsuario"
+                onChange={(event) => {setName(event.target.value)}}  
+                placeholder="Digite seu nome"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar 
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
